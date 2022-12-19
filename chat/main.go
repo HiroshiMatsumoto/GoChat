8f3c2e907564ae15a736a5605a17c4f6498d5d03
matsumoto <h_matsumoto@hotmail.com>
2022-12-19 19:26:35 +0900

    add profile picture as avatar

--- chat/main.go
+++ chat/main.go
@@ -71,6 +71,16 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "", // for the case MaxAge isn't supported
+			Path:   "/",
+			MaxAge: -1, // expires immideately
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	// up chatroom
 	go r.run()
 	// run webserver
